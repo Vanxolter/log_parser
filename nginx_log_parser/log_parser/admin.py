70df74a71ba36fa84cd084fabc7eeeaaa6160fc5
vanxolter <Vansheltor@gmail.com>
2024-09-03 20:34:10 +0300

    designed the base model LogEntry in models.py

--- nginx_log_parser/log_parser/admin.py
+++ nginx_log_parser/log_parser/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from log_parser.models import LogEntry
 
-# Register your models here.
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'timestamp', 'http_method', 'uri', 'response_code')
+    search_fields = ('ip_address', 'uri')
+    list_filter = ('http_method', 'response_code')
--- nginx_log_parser/log_parser/models.py
+++ nginx_log_parser/log_parser/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class LogEntry(models.Model):
+    """
+    Модель данных из лога.
+
+    Атрибуты
+        ip_address (GenericIPAddressField): IP адрес.
+        timestamp (DateTimeField): Время запроса.
+        http_method (str): http метод (GET, POST,...).
+        uri (str): URI запроса.
+        response_code (int): Код ответов.
+        response_size (int): Размер ответа.
+    """
+
+    ip_address = models.GenericIPAddressField(
+        verbose_name='IP Адрес',
+        db_comment='IP адрес юзера оценивающего рецензию, защита от накрутки'
+    )
+    timestamp = models.DateTimeField(
+        verbose_name='Время запроса',
+        db_comment='Время запроса из лога',
+        null=True,
+        blank=True,
+    )
+    http_method = models.CharField(
+        max_length=10,
+        default='',
+        verbose_name='http метод',
+        db_comment='http метод (GET, POST,...)'
+    )
+    uri = models.CharField(
+        max_length=255,
+        default='',
+        verbose_name='URI запроса',
+        db_comment='URI запроса'
+    )
+    response_code = models.IntegerField(
+        default=0,
+        verbose_name='Код ответа',
+        db_comment='Код ответа из лога'
+    )
+    response_size = models.IntegerField(
+        default=0,
+        verbose_name='Размер ответа',
+        db_comment='Размер ответа из лога'
+    )
+
+    def str(self):
+        return f"{self.ip_address} - {self.http_method} {self.uri}"
+
+    class Meta:
+        db_table = 'log_data'
+        indexes = [models.Index(
+            fields=['-timestamp', 'ip_address'])]
+        ordering = ['-timestamp']
+        verbose_name = 'Запрос'
+        verbose_name_plural = 'Запросы'
